e2b0281f3642cbdc13d777af7234c8e658d45bec
lokesh <lokesh.p@acurussolutions.com>
2024-02-28 16:41:34 +0530

    Initial Commit

---
+++ Letcode/src/java_learner/DataTypes.java
@@ -0,0 +1,25 @@
+package java_learner;
+
+public class DataTypes {
+	public static void main(String[] args) {
+		byte rollNo = 46;
+		String name = "Santhosh Kumar";
+		short salary = 10000;
+		int number = 986528748;
+		double height = 172.2;
+		float weight = 65.5f;
+		char grade = 'B';
+		boolean status = true;
+		System.out.println("RollNo :" + rollNo);
+		System.out.println("Name :" + name);
+		System.out.println("Salary :" + salary);
+		System.out.println("Number :" + number);
+		System.out.println("Height :" + height);
+		System.out.println("Weight :" + weight);
+		System.out.println("Garde :" + grade);
+		System.out.println("Pass : " + status);
+		
+		
+	}
+
+}
